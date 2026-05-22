200eaa7568d88a5c630a80f4279f9303c2d5f14a
Ferrmon <genibhan@gmail.com>
2026-05-22 14:41:00 +0545

    Removed unnecessary folder

--- server/blankcodemodules.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<unordered_map>
-using namespace std;
-
-int main(){
-    unordered_map<string,string> usersAndPublicKeys;
-    // On connection recieve user name and their public keys (I have no idea how to do it frontend n networking dude)
-    
-}
